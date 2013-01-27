de74673832868014bf6f85909aed7a799d24c348
Pablo Recio <pablo@recio.me>
2013-01-27 19:34:32 +0000

    Adding links model

--- hendrix/blog/models.py
+++ hendrix/blog/models.py
@@ -4,6 +4,14 @@ from django.utils.translation import ugettext_lazy as _
 from autoslug import AutoSlugField
 
 
+class WeightMixin(models.Model):
+    weight = models.IntegerField(
+        verbose_name=_(u'Weight'),
+        help_text=_(u'For proper ordering with it\'s siblings'),
+        default=0
+    )
+
+
 class Tag(models.Model):
     name = models.CharField(verbose_name=_(u'Name'), max_length=100)
     slug = AutoSlugField(populate_from='name', unique=True)
@@ -14,3 +22,20 @@ class Tag(models.Model):
     class Meta:
         verbose_name = _(u'Tag')
         verbose_name_plural = _(u'Tags')
+
+
+class Link(WeightMixin, models.Model):
+    title = models.CharField(verbose_name=_(u'Title'), max_length=100)
+    url = models.CharField(
+        verbose_name=_(u'URL'),
+        max_length=400,
+        help_text=_(u'Relative if you want an internal link, full link otherwise')
+    )
+
+    def __unicode__(self):
+        return u'%s - <%s>' % (self.title, self.url)
+
+    class Meta:
+        verbose_name = _(u'Link')
+        verbose_name_plural = _(u'Links')
+        ordering = ["weight"]
--- hendrix/blog/tests/models_tests.py
+++ hendrix/blog/tests/models_tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from ..models import Tag
+from ..models import Link, Tag
 
 
 class TagTestCase(TestCase):
@@ -23,3 +23,28 @@ class TagTestCase(TestCase):
 
         tag_3 = Tag.objects.create(name="Metal")
         self.assertEqual(tag_3.slug, "metal-2")
+
+
+class LinkTestCase(TestCase):
+
+    def test_unicode(self):
+        link_1 = Link.objects.create(title="About", url="/about")
+        self.assertEqual(unicode(link_1), u"About - </about>")
+        self.assertEqual(str(link_1), "About - </about>")
+
+        link_2 = Link.objects.create(title="Google", url="http://www.google.com")
+        self.assertEqual(unicode(link_2), u"Google - <http://www.google.com>")
+        self.assertEqual(str(link_2), "Google - <http://www.google.com>")
+
+    def test_weight(self):
+        link_1 = Link.objects.create(
+            title="About", url="/about", weight=2)
+        link_2 = Link.objects.create(
+            title="Google", url="http://www.google.com", weight=1)
+        link_3 = Link.objects.create(
+            title="Links", url="/links", weight=3)
+
+        links = Link.objects.all()
+        self.assertEqual(link_2, links[0])
+        self.assertEqual(link_1, links[1])
+        self.assertEqual(link_3, links[2])
