e175156bcee5c396ef3b2afcd7317c9c48eb3c0d
Pablo Recio <pablo@recio.me>
2013-02-22 23:09:46 +0000

    Added basic about page

--- hendrix/hendrix/urls.py
+++ hendrix/hendrix/urls.py
@@ -6,5 +6,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
+    url(r'^about/$', TemplateView.as_view(template_name="about.html"), name='about'),
     url(r'^admin/', include(admin.site.urls)),
 )
