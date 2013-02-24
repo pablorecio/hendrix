9f80608a214e997e91a5e08a656b29991a86ea62
Pablo Recio <pablo@recio.me>
2013-02-24 20:03:47 +0000

    Added analytics

--- hendrix/hendrix/urls.py
+++ hendrix/hendrix/urls.py
@@ -1,11 +1,6 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
-admin.autodiscover()
-
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
-    url(r'^about/$', TemplateView.as_view(template_name="about.html"), name='about'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name="about.html"), name='index'),
 )
